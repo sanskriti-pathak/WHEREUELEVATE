76deeb60ff94a2baec214fdd756f66b428707429
Sanskriti Pathak <161308341+sanskriti-pathak@users.noreply.github.com>
2024-12-15 17:50:10 +0530

    js classes

---
+++ modules/data .js
@@ -0,0 +1,44 @@
+export const defaultData = {
+    university: {
+        name: "Future University",
+        studentsCount: 1500,
+        isCollegeOpen: true,
+    },
+    courses: {
+        BTech: {
+            name: "B.Tech",
+            duration: "4 Years",
+            fee: 70000,
+            students: 1500,
+            hod: "Dr. Abhishek Saxena",
+        },
+        BCA: {
+            name: "B.C.A",
+            duration: "3 Years",
+            fee: 35000,
+            students: 700,
+            hod: "Mr. Sachin Raj Saxena",
+        },
+        MCA: {
+            name: "M.C.A",
+            duration: "2 Years",
+            fee: 45000,
+            students: 650,
+            hod: "Mr. Sachin Raj Saxena",
+        },
+        BPharma: {
+            name: "B.Pharma",
+            duration: "4 Years",
+            fee: 110000,
+            students: 1800,
+            hod: "Dr. Rahul Shukla",
+        },
+        MBA: {
+            name: "MBA",
+            duration: "2 Years",
+            fee: 50000,
+            students: 500,
+            hod: "Mr. Virendra",
+        }
+    }
+};
