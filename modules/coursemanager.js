1aaa74c2c15e118a42b85c8e30e3182694bbba69
Sanskriti Pathak <161308341+sanskriti-pathak@users.noreply.github.com>
2024-12-15 17:39:52 +0530

    object

--- modules/coursemanager.js
+++ modules/coursemanager.js
@@ -7,3 +7,12 @@ export default class CourseManager {
             return acc;
         }, {});
     }
+
+    calculateTotalFee() {
+        try {
+            return Object.values(this.courses).reduce((total, course) => total + course.fee, 0);
+        } catch (error) {
+            console.error(`Error in calculateTotalFee: ${error.message}`);
+            return 0;
+        }
+    }
