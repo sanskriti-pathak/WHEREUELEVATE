aee3c3f43bf3cd0c5a37e3f035f92753c0e149d3
Sanskriti Pathak <161308341+sanskriti-pathak@users.noreply.github.com>
2024-12-15 17:54:02 +0530

    modules

--- modules/ui.js
+++ modules/ui.js
@@ -2,3 +2,15 @@ export const updateWelcomeMessage = () => {
     const welcomeElement = document.querySelector('.welcome-message');
     if (welcomeElement) welcomeElement.textContent = 'Welcome to our University Portal';
 };
+
+export const populateCoursesList = (courses) => {
+    const coursesList = document.getElementById('coursesList');
+    if (coursesList) {
+        coursesList.innerHTML = '';
+        Object.keys(courses).forEach(course => {
+            const listItem = document.createElement('li');
+            listItem.textContent = course;
+            coursesList.appendChild(listItem);
+        });
+    }
+};
